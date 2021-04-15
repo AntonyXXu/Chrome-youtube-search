408d667b97761fa15ec4d7fc95205bacc65e9444
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-15 17:01:26 -0600

    added readme, added youtube to search

--- YoutubeSearch.js
+++ YoutubeSearch.js
@@ -1,29 +1,34 @@
 // import  from "./assets/";
 
-//Check if current host is google
-const googleHost = "www.google.";
-const currentURL = window.location.hostname;
-if (currentURL.search(googleHost) == -1) {
-  return;
+function addYoutubeSearch() {
+  //Check if current host is google
+  const googleHost = "www.google.";
+  const currentURL = window.location.hostname;
+  if (currentURL.search(googleHost) == -1) {
+    return;
+  }
+
+  //Google is in the host name, find the search tabs
+  //Google search class MUFPAc includes all search tabs, class hdtb-mitem includes individual search tabs
+  const searchTabs = document.getElementsByClassName("MUFPAc");
+
+  // Clone a node in the search tab to insert the youtube search
+  const original = searchTabs[0].childNodes[1];
+  let youtubeClone = original.cloneNode(true);
+
+  //Change icon and data within the clone
+  youtubeClone.getElementsByTagName("a")[0].text = "YouTube";
+
+  //Get the current search, then send the query to YouTube in a new tab
+  const currentSearch = window.location.search;
+  var query = currentSearch.substring(
+    currentSearch.indexOf("?q=") + 1,
+    currentSearch.indexOf("&")
+  );
+  var query = "https://www.youtube.com/results?search_query=" + query;
+  youtubeClone.getElementsByTagName("a")[0].href = query;
+  youtubeClone.getElementsByTagName("a")[0].target = "_blank";
+
+  //Add the new tab into the search
+  original.insertAdjacentElement("afterend", youtubeClone);
 }
-
-//Google is in the host name, find the search tabs
-//Google search class MUFPAc includes all search tabs, class hdtb-mitem includes individual search tabs
-const searchTabs = document.getElementsByClassName("MUFPAc");
-
-// Clone a node in the search tab to insert the youtube search
-const original = searchTabs[0].childNodes[1];
-let youtubeClone = original.cloneNode(true);
-
-//Change icon and data within the clone
-youtubeClone.getElementsByTagName("a")[0].text = "YouTube";
-
-//Get the current search, then send the query to YouTube in a new tab
-const currentSearch = window.location.search;
-var query = currentSearch.split("?q=")[1].split("&")[0];
-var query = "https://www.youtube.com/results?search_query=" + query;
-youtubeClone.getElementsByTagName("a")[0].href = query;
-youtubeClone.getElementsByTagName("a")[0].target = "_blank";
-
-//Add the new tab into the search
-original.insertAdjacentElement("afterend", youtubeClone);
