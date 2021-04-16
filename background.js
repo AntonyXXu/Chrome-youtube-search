97813ffa3871f2089dca23fe0b21f113c3c72fda
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-15 20:40:44 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 try {
+  //If change, run YoutubeSearch.js
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     chrome.scripting.executeScript({
       files: ["YoutubeSearch.js"],
